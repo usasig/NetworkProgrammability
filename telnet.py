8bed7a30d40252f673a043893e7f27b680889ebc
sasig <sasig@cisco.com>
2017-09-12 21:59:05 -0400

    summary
    This is a python program does the Telnet to Cisco Routers

---
+++ telnet.py
@@ -0,0 +1,24 @@
+import getpass
+import sys
+import telnetlib
+
+HOST = "192.168.0.10"
+user = raw_input("Enter your username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("rname: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("en\n")
+if password:
+	tn.read_until("assword: ")
+	tn.write(password + "\n")
+	
+tn.write("show ver\n")
+
+print tn.read_all()
